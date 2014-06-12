0a68b588dcf1b087f4e00bf59992aff801fcf39f
Ubuntu <ubuntu@ip-10-72-218-212.ec2.internal>
2014-06-12 20:59:09 +0000

    Added a couple of tests.

--- test/test_sparql_model.rb
+++ test/test_sparql_model.rb
@@ -1,5 +1,22 @@
 require 'test/unit'
-require 'sparql_model'
+require_relative '../lib/image'
 
 class SparqlModelTest < Test::Unit::TestCase
+  
+  def test_simple
+    assert_equal( 2, 1+1 )
+  end
+  
+  def test_create
+    img = Image.new
+    img.create({ :path => 'check' })
+    assert_equal( 'check', img.path )
+  end
+  
+  def test_new_get
+    img = Image.new( 'check' )
+    assert_equal( 'check', img.path )
+  end
+  
+  
 end
