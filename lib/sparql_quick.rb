f548d13ecfff5fbba634a8ba5e27ea4bcede1b14
caesarfeta <adamtavares@gmail.com>
2014-08-11 11:35:06 -0400

    Improved tagify method, so URN mistakes can be made quietly.

--- lib/sparql_quick.rb
+++ lib/sparql_quick.rb
@@ -266,19 +266,33 @@ end
 
 
 class String
+  
   # Clip the first and last characters from a string
   # @return { String }
   def clip
     self[1..-2]
   end
   
-  # Add opening and closing tags
-  def tagify
-    "<#{self}>"
-  end
-  
   # Check to see if we're looking at an integer in string's clothing
   def is_i?
      !!( self =~ /\A[-+]?[0-9]+\z/ )
   end
+  
+  # Return integer
+  def just_i
+    /\d+/.match( self ).to_s.to_i
+  end
+  
+  # Wrap <>
+  def tagify
+    this = self
+    if this[0] != "<"
+      this = "<#{this}"
+    end
+    if this[-1,1] != ">"
+      this = "#{this}>"
+    end
+    this
+  end
+  
 end
